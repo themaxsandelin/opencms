08072faea53bd582b9aa3e88bad19db748e60f9f
Max Sandelin <themaxsandelin@gmail.com>
2022-09-15 14:31:37 +0200

    Change default route to 3100 + add editorconfig

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 // Dependencies
-import express, { Express, Request, Response } from 'express';
+import express, { Express } from 'express';
 import dotenv from 'dotenv';
 
 // Main router
@@ -8,7 +8,7 @@ import router from './routes';
 dotenv.config();
 
 const app: Express = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3100;
 
 app.use(router);
 
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,7 +4,7 @@ import { Router, Request, Response } from 'express';
 const router = Router();
 
 router.get('/', (req: Request, res: Response) => {
-  res.json({ message: 'Hello, world!' });
+  res.json({ message: 'Do, or do not. There is no try.' });
 });
 
 export default router;
