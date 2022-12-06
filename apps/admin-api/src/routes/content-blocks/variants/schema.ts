302763135f7b51d5b5541d843d43009d71dcb526
Max Sandelin <themaxsandelin@gmail.com>
2022-12-06 22:38:49 +0100

    Make it possible to update content block variants

--- apps/admin-api/src/routes/content-blocks/variants/schema.ts
+++ apps/admin-api/src/routes/content-blocks/variants/schema.ts
@@ -4,8 +4,20 @@ import { z } from 'zod';
 export const createVariantSchema = z.object({
   body: z.object({
     name: z.string({
-      required_error: 'You have to give the content block a name.'
+      required_error: 'You have to give the content block a name.',
+      invalid_type_error: 'The name must be of type string.'
     }),
+    sites: z.string({
+      invalid_type_error: 'The list of site IDs must be of type string.'
+    }).array().optional()
+  })
+});
+
+export const patchVariantSchema = z.object({
+  body: z.object({
+    name: z.string({
+      invalid_type_error: 'The name must be of type string.'
+    }).optional(),
     sites: z.string({
       invalid_type_error: 'The list of site IDs must be of type string.'
     }).array().optional()
